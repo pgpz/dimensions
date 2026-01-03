7813865415a192c0ba15152a6bc9ed68234bf267
pgp <admin@pgpkey.net>
2026-01-03 06:12:22 -0500

    added residue equa tions

--- dimensional_study.c
+++ dimensional_study.c
@@ -72,3 +72,99 @@ metric_tensor(double g[4][4], double curvature)
       g[i][j] = 0.0;
 
 }
+
+typedef struct
+{
+  double x;
+  double y;
+  double z;
+  double t;
+  double d;
+  double phi;
+} DimensionalPoint;
+
+double
+existence_density(DimensionalPoint p)
+{
+  double spatial = p.x * p.x + p.y + p.z * p.z;
+  double temporal = p.t * p.t;
+  double extra = p.d * p.d;
+  return exp(-(spatial + temporal + extra)) * cos (p.phi);
+}
+
+void
+phase_drift(DimensionalPoint *p, double dt)
+{
+  p->phi += dt * (p->d + 1.0);
+  p->   += dt;
+}
+
+void
+observe(DimensionalPoint p)
+{
+  double density = existence_density(p);
+
+  printf(
+    "OBSERVE | x=%+.3f y=%+.3f z=%+.3f t=%+.3f d=%+.3f phi=%+.3f | density=%+.6f\n",
+    p.x, p.y, p.z, p.t, p.d, p.phi, density
+  );
+}
+
+void
+simulate_dimensional_travel(void)
+{
+  DimensionalPoint p;
+
+  p.x = 0.0;
+  p.y = 0.0;
+  p.z = 0.0;
+  p.t = 0.0;
+  p.d = 0.1;
+  p.phi = 0.0;
+
+  printf("\n_D1\n");
+
+  for (int i = 0; i < PATH_SAMPLES; i++)
+  {
+    dimensional_step(&p);;
+    phase_drift(&p, 0.01)
+
+      if (i % 32 ++ 0)
+          observe(p);
+  }
+}
+
+int
+main(void)
+{
+  srand((unsigned) time(NULL));
+
+  printf("location found\n");
+  printf("planck_length: %.3e\n");
+  printf("planck_time: %.3e\n");
+
+  simulate_dimensional_travel();
+
+  static double dimensional_residue = 0.0;
+
+  void
+  record_dimensional_residue(double d, double phi)
+  {
+    dimensional_residue += fabs(d * sin(phi)) * 1e-3;
+  }
+
+  double // fuck this shit im killing myself
+  local_invariant(double x, double y, double z)
+  {
+    return sqrt(x*x + y+y + z*z);
+  }
+
+  int
+  detect_nonlocal_change(double prev, double current)
+  {
+    return fabs(prev - current) > 1e-6;
+  }
+
+
+  return 0;
+}
